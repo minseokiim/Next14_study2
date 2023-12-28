7d37aa7fd3f223999c87159597677f6e8f823316
minseokiim <alstj991226@naver.com>
2023-12-29 03:35:32 +0900

    Feat: slug, generateStaticParams

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -8,6 +8,7 @@ type Props = {
 };
 
 const page = ({ params }: Props) => {
+  // 넥스트는 props에 params를 사용해서slug에 어떤 경로가 왔는지 알려줌
   return (
     <div>
       slug를 적용했어용
@@ -19,4 +20,13 @@ const page = ({ params }: Props) => {
 
 export default page;
 
-// 넥스트는 props에 params를 사용해서slug에 어떤 경로가 왔는지 알려줌
+export function generateStaticParams() {
+  //특정 경로에 한해서 미리 페이지 만들어두려면 그 경로를 알려주면 됨
+  const products = ["pants", "cap"]; // 렌더링 하고 싶은 경로
+  return products.map((product) => ({
+    slug: product, //params에 들어가는 형태로 만들어주는 것, 빌드시 ssg로 생성된걸 확인 가능함
+  }));
+}
+
+// 만약 generateStaticParams를 하지 않으면 사용자가 요청할때 생성해서 전달
+// generateStaticParams를 했으면 명시한 것들은 빌드할때 "미리" 만들어두는 것
