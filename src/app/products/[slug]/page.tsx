b261f0672329131ee88db77dd1dfeebb03e3ed77
minseokiim <alstj991226@naver.com>
2023-12-30 05:39:48 +0900

    Feat: ISR 렌더링- fetch

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -12,24 +12,18 @@ type Props = {
 
 //제품별로 동적인 메타데이터 만들기
 export function generateMetadata({ params }: Props) {
-  //어떤 경로에 있는지 표시해줘야하니 params받기
-
   return {
     title: `이름 ${params.slug}`,
   };
 }
 
 const page = async ({ params: { slug } }: Props) => {
-  // 넥스트는 props에 params를 사용해서slug에 어떤 경로가 왔는지 알려줌
-  //구조분해 할당으로 바꿔줌
-
   const product = await getProduct(slug);
 
   return (
     <div>
       slug를 적용했어용
       <br />
-      {/* 리팩토링 -> 서버 파일에 있는 데이터 중 해당정보 읽어오기 */}
       경로는 {product?.name} 입니당
     </div>
   );
@@ -40,16 +34,8 @@ export default page;
 export async function generateStaticParams() {
   //특정 경로에 한해서 미리 페이지 만들어두려면 그 경로를 알려주면 됨
 
-  {
-    /* 리팩토링 -> 모든 페이지 미리 만들기 */
-  }
-
   const products = await getProducts();
-  // const products = ["pants", "cap"]; // 렌더링 하고 싶은 경로
   return products.map((product) => ({
-    slug: product.id, //params에 들어가는 형태로 만들어주는 것, 빌드시 ssg로 생성된걸 확인 가능함
+    slug: product.id,
   }));
 }
-
-// 만약 generateStaticParams를 하지 않으면 사용자가 요청할때 생성해서 전달
-// generateStaticParams를 했으면 명시한 것들은 빌드할때 "미리" 만들어두는 것
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,21 +3,24 @@ import Link from "next/link";
 import os from "os"; //node api중 하나
 import { getProducts } from "./../../api/products";
 
-export const revalidate = 3; //ISR, 3초마다
+//export const revalidate = 3; //이걸 주석하면 아무리 새로고침해도 안바뀜-> fetch에서 시간 설정 가능
 
 const page = async () => {
-  //const products = ["shirt", "pants"]; //리팩토링 -> 서버 파일에 있는 리스트 읽어오기
-
-  //promise를 할당해주니까 에러가 나는 것.await해주기
   const products = await getProducts();
 
   console.log("서버 컴포넌트 테스트");
   //웹 콘솔에는 안찍히고 vscode터미널에만 찍힘
-  //서버컴포넌트는 서버에서 실행이 됨
-  //이 코드는 서버에서 실행되어 pre-rendering된 html 페이지로 만들어서 html이므로 콘솔이 안찍히는 것
-  //서버에서 동작하므로 브라우저에서 제공하는 api사용이 불가능하지만, node api는 사용가능
   console.log(os.hostname());
   // 서버 컴포넌트에서는 서버상의 파일 접근 가능
+
+  const res = await fetch("https://meowfacts.herokuapp.com", {
+    // next: { revalidate: 0 }, //ISR,
+    // 0이면 요청 올때마다 revalidate -> SSR
+    cache: "no-store", //캐쉬하지 않는단 뜻-> SSR처럼 행동, revalidate: 0과 같은 의미
+  });
+
+  const data = await res.json();
+  const factText = data.data[0];
 
   return (
     <>
@@ -31,6 +34,7 @@ const page = async () => {
           );
         })}
       </ul>
+      <article>{factText}</article>
     </>
   );
 };
