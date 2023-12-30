9ad673307f107991516719017d30ac0a25569ce8
minseokiim <alstj991226@naver.com>
2023-12-31 06:52:06 +0900

    Feat: 폰트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nanum_Gothic, Open_Sans } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+const sans = Open_Sans({ subsets: ["latin"] });
+const gothic = Nanum_Gothic({
+  weight: "700",
+  subsets: ["latin"],
+});
 
 // SEO
 // layout.js에 metadata를 정의하면 해당 경로의 페이지 뿐만 아니라 자식라우트에서도 재사용됨
@@ -25,14 +29,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <header>
-          <h2>
+          <h2 className={sans.className}>
             ./layout 에서 지정한 헤더입니당 <hr />
           </h2>
         </header>
         <nav>
-          <h3>
+          <h3 className={gothic.className}>
             ./layout 에서 지정한 냅바입니당
             <hr />
           </h3>
